fa396cfc3ebb20ac19487fe037d75fd40752af86
Elliot Saba <staticfloat@gmail.com>
2019-10-02 18:47:27 -0400

    Librsvg_jll build 2.42.2+0

---
+++ src/Librsvg_jll.jl
@@ -0,0 +1,31 @@
+module Librsvg_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("925c91fb-5dd6-59dd-8e8c-345e74382d89"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Librsvg", artifacts_toml) for e in artifacts["Librsvg"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Librsvg; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Librsvg_jll
