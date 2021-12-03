720e519f3119bb2a804d51f5b8ee0eabd0cc7efe
jlbuild <juliabuildbot@gmail.com>
2021-12-03 23:27:19 +0000

    Librsvg_jll build 2.52.4+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -3,13 +3,12 @@ export libpixbufloader_svg, librsvg, rsvg_convert
 
 using gdk_pixbuf_jll
 using Pango_jll
-using Libcroco_jll
 JLLWrappers.@generate_wrapper_header("Librsvg")
 JLLWrappers.@declare_file_product(libpixbufloader_svg)
 JLLWrappers.@declare_library_product(librsvg, "librsvg-2.so.2")
 JLLWrappers.@declare_executable_product(rsvg_convert)
 function __init__()
-    JLLWrappers.@generate_init_header(gdk_pixbuf_jll, Pango_jll, Libcroco_jll)
+    JLLWrappers.@generate_init_header(gdk_pixbuf_jll, Pango_jll)
     JLLWrappers.@init_file_product(
         libpixbufloader_svg,
         "lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.so",
